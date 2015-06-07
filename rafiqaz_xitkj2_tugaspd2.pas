0f74531d399de8d5547f211f325d864fb9c7f61a
RafiqAZ <zaini_ahmad29@yahoo.com>
2015-06-07 20:21:55 +0700

    tugaspd2

---
+++ rafiqaz_xitkj2_tugaspd2.pas
@@ -0,0 +1,48 @@
+unit RafiqAZ_XItkj2_tugaspd2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  label1.caption:=edit1.text;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  label1.caption:='';
+  edit1.clear;
+end;
+
+end.
+
